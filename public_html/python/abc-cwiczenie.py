57d31ece3457148073f7dfacd80674c18d1decf3
majaharkot <harkot.majka@gmail.com>
2018-09-19 07:27:04 +0200

    abc poprawki

--- public_html/python/abc-cwiczenie.py
+++ public_html/python/abc-cwiczenie.py
@@ -7,46 +7,38 @@
 # a następnie wyświetla liczbę największą.
 
 def maks(a, b, c):
+    m = None
     if a > b:
         if a > c:
             m = a
         else:
-			m = c 
+            m = c 
     elif b > c:
             m = b
     print("Największa:", m)
     
     return m
 
+def maks2(a, b, c):
+    maks = a
+    if b > maks:
+        maks = b
+    if c > maks:
+        maks = c
+        
+    return maks
+
 def main(args):
     
-    assert(maks(3, 2, 1) == 3)
-    assert(maks(3, 1, 2) == 3)
-    assert(maks(3, 1, 3) == 3)
-    assert(maks(3, 2, 3) == 3)
-    assert(maks(3, 2, 2) == 3)
-    assert(maks(3, 1, 1) == 3)
-    assert(maks(3, 3, 1) == 3)
-    assert(maks(3, 3, 2) == 3)
-    assert(maks(3, 3, 3) == 3)
-    assert(maks(2, 1, 3) == 3)
-    assert(maks(2, 3, 1) == 3)
-    assert(maks(2, 1, 2) == 3)
-    assert(maks(2, 3, 2) == 3)
-    assert(maks(2, 2, 1) == 3)
-    assert(maks(2, 2, 3) == 3)
-    assert(maks(2, 1, 1) == 3)
-    assert(maks(2, 3, 3) == 3)
-    assert(maks(2, 2, 2) == 3)
-    assert(maks(1, 2, 3) == 3)
-    assert(maks(1, 3, 2) == 3)
-    assert(maks(1, 2, 2) == 3)
-    assert(maks(1, 3, 3) == 3)
-    assert(maks(1, 1, 3) == 3)
-    assert(maks(1, 1, 2) == 3)
-    assert(maks(1, 1, 1) == 3)
-    assert(maks(1, 2, 1) == 3)
-    assert(maks(1, 3, 1) == 3)
+    assert(maks2(3, 2, 1) == 3)
+    assert(maks2(2, 3, 1) == 3)
+    assert(maks2(1, 2, 3) == 3)
+    assert(maks2(1, 1, 3) == 3)
+    assert(maks2(3, 1, 1) == 3)
+    assert(maks2(1, 3, 1) == 3)
+    assert(maks2(1, 3, 3) == 3)
+    assert(maks2(3, 3, 1) == 3)
+    assert(maks2(3, 3, 3) == 3)    
 
     return 0
 
