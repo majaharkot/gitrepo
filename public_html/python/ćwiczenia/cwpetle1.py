9ed72e79b8d97107a06ced8f48d1983ec0014150
majaharkot <harkot.majka@gmail.com>
2018-09-25 14:21:15 +0200

    pętle w pythonie ćwiczenia

---
+++ public_html/python/ćwiczenia/cwpetle1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  cwpetle1.py
+#  
+
+def main(args):
+    
+    wynik = 0
+    
+    while wynik >= 75:
+        print(wynik)
+
+        
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
