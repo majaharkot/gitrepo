ef2559edd0bafe00e779b9b9641efb12e430e650
majaharkot <harkot.majka@gmail.com>
2018-10-09 14:20:31 +0200

    python - różniutkie programiki

---
+++ public_html/python/liczby2-3.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  liczby2-3.py
+# 
+
+
+def liczby2(a=10, b=99):
+    
+    """
+    Program drukuje wszystkie liczby dwucyfrowe o niepowtarzających się
+    cyfrach oraz zwraca ilość takich liczb
+    """
+    for i in range(a, b):
+        for j in range(a, b):
+            
+        
+    
+def liczby3(a=100, b=999):
+    
+    """
+    Program drukuje wszystkie liczby trzycyfrowe o niepowtarzających się
+    cyfrach oraz zwraca ilość takich liczb
+    """
+
+
+
+def main(args):
+    
+    print(liczby2)
+    print(liczby3)
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
