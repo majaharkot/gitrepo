e06c9f9d0548f8ce2cac7e97300118ac04e6066e
majaharkot <harkot.majka@gmail.com>
2018-10-16 13:40:39 +0200

    silnia i największy wspólny dzielnik

---
+++ public_html/python/nwd_euklides.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  nwd_euklides.py
+#  
+
+def nwd(a,b):
+    while a != b:
+        if a > b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+    
+def main(args):
+    
+    a = int(input("Podaj liczbę: "))
+    b = int(input("Podaj liczbę: "))
+    
+    print("NWD {} i {} jest {} ". format(a, b, nwd(a, b)), end='')
+    
+    return 0
+
+if __name__ == '__main__':
+    import sys
+sys.exit(main(sys.argv))
