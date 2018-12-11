8d6776e53809086cb4b0b219a618fd2858b23e87
majaharkot <harkot.majka@gmail.com>
2018-12-11 14:22:39 +0100

    sortowanie bąbelkowe i przez wstawianie

--- public_html/CPP/sortowanie.cpp
+++ public_html/CPP/sortowanie.cpp
@@ -25,25 +25,59 @@ void drukuj(int tab[], int roz)
     }
 }
 
-void zamien(int a, int b)
+
+void zamien(int &a, int &b)
 {
-    
-    
+    int tmp; //zmienna pomocnicza
+    tmp = a;
+    a = b;
+    b = tmp;
 }
 
-void sort_bubble(int tab[], int n)
+void zamien2(int tab[], int i)
 {
-    cout << "\nSortowanie bąbelkowe:\n"
-    
-    for(int j = n-1; j > 0 ; j--)
+    int tmp = tab[i];
+    tab[i] = tab[i+1];
+    tab[i+1] = tmp;
+}
+
+void bubble_sort(int tab[],int n)
+{
+    cout << "\nSortowanie bąbelkowe\n";
+    int licznik = 0;
+    for(int j = n - 1; j > 0; j--)
     {
-        for(int i = 0, i < j; i++)
+        for(int i = 0; i <  j; i++) //pętla wewnętrzna
         {
-            if(tab[i]>tab[i+1])
-                zamien(tab[i], tab[i+1])
+            licznik++;
+            if(tab[i] > tab[i+1])
+            {
+                //zamiana miejscami
+                zamien2(tab, i);
+            }
         }
-    }   
-    
+    }
+    cout << "Powtórzenia: " << licznik << endl;
+}
+
+void insert_sort(int tab[], int n)
+{
+    cout << "\nSortowanie przez wstawianie:\n";
+     
+    int i, j, tmp;
+     
+    for(i = 1; i < n; i++)
+    {
+        tmp = tab[1];
+        j = i - 1;
+        while (j >= 0 && tab[j] > tmp)
+        {
+            tab[j+1] = tab[j];
+            j--;
+        }
+        tab[j=1] = tmp;
+    }
+     
 }
 
 int main(int argc, char **argv)
@@ -52,6 +86,11 @@ int main(int argc, char **argv)
     int tablica[rozmiar]; //statyczna deklaracja tablicy
     
     wypelnij_losowe(tablica, rozmiar);
+    drukuj(tablica, rozmiar);
+    cout << endl;
+    bubble_sort(tablica, rozmiar);
+    drukuj(tablica, rozmiar);
+    insert_sort(tablica, rozmiar);
     drukuj(tablica, rozmiar);
         
 	return 0;
