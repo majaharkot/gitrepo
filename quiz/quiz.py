2da2a6a67147cdd75a15b371a68d1ffa9c6f4b92
majaharkot <harkot.majka@gmail.com>
2018-12-14 08:45:40 +0100

    aplikacja quiz dodawanie danych łuuuhuu

--- quiz/quiz.py
+++ quiz/quiz.py
@@ -5,19 +5,30 @@
 #
 #  Running on http://127.0.0.1:5000/  
 
-from flask import Flask
-from flask import render_template
+from flask import Flask, g
+from flask import render_template, request
+from modele import *
+
 app = Flask(__name__)
 
-print(__name__)
+@app.before_request
+def before_request():
+    g.db = baza
+    g.db.connect()
+    
+@app.after_request
+def after_request(response):
+    g.db.close()
+    return response
 
 @app.route("/")
 def hello():
     return "<h1>Witaj na serwerze!</h1><h2>Aplikacja quiz</h2>"
     
-@app.route("/strona") #definicja zasobu
-def strona():
-    return render_template('strona.html')
+@app.route("/quiz") #definicja zasobu
+def quiz():
+    pytania = Pytanie.select()
+    return render_template('quiz.html', query = pytania)
     
 @app.route("/klasa")
 def klasa():
