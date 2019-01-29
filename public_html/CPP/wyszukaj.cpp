15019753bcf77cc55bb96d5ff8a11085bb6a7f3e
majaharkot <harkot.majka@gmail.com>
2019-01-29 14:19:38 +0100

    wyszukiwanie liczby w losowej tablicy

---
+++ public_html/CPP/wyszukaj.cpp
@@ -0,0 +1,85 @@
+/*
+ * wyszukaj.cpp 
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+void wypelnij_losowe(int tab[], int roz)
+{   
+    srand(time(NULL));  // inicjacja generatora pseudolosowych
+    
+    for (int i = 0; i < roz; i++)
+    {
+        tab[i] = rand() % 101;  
+    }
+}
+
+void drukuj(int tab[], int roz)
+{    
+    for (int i = 0; i < roz; i++)
+    {
+        cout << tab[i] << ' ';
+    }
+}
+
+void insert_sort(int tab[], int n)
+{
+    cout << "\nSortowanie przez wstawianie:\n";
+
+    int i, j, tmp;
+     
+    for(i = 1; i < n; i++)
+    {
+        tmp = tab[i];
+        j = i - 1;
+        while (j >= 0 && tab[j] > tmp)
+        {
+            tab[j+1] = tab[j];
+            j--;
+        }
+        tab[j+1] = tmp;
+    }
+}
+
+int szukaj_bin_it(int tab[], int n, int szuk)
+{
+    int p, k, s;
+    p = 0, k = n - 1;
+    
+    while (p <= k)
+    {
+        s = (p + k) / 2;
+        if (tab[s] == szuk) return s;
+        else if (szuk < tab[s]) k = s - 1;
+        else p = s + 1;
+    }
+    return -1;
+    
+}
+
+int main(int argc, char **argv)
+{
+    int n = 20;
+    int tab[n];
+    
+    wypelnij_losowe(tab, n);
+    drukuj(tab, n);
+    
+    int szuk = 0;
+    
+    cout << "\nPodaj liczbę: ";
+    cin >> szuk;
+    insert_sort(tab, n);
+    drukuj(tab, n);
+    
+    int indeks = szukaj_bin_it(tab, n, szuk);
+    
+    if (indeks>= 0)
+        cout<< "\nZnaleziona na indeksie " << indeks << endl;
+    else
+        cout << "\nNie znaleziona! " << endl;
+}
+
