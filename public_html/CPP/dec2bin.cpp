d11a7e2ef98cb6c5928824f2f48299e72132dd09
majaharkot <harkot.majka@gmail.com>
2019-02-04 23:29:15 +0100

    bin to dec - i don no

--- public_html/CPP/dec2bin.cpp
+++ public_html/CPP/dec2bin.cpp
@@ -8,35 +8,64 @@
 #include <iostream>
 using namespace std;
 
-int dec2bin(int l)
+int dec2bin(int ld)
 {
     int lbin = 0;
     int i = 1;
     
-    while (l > 0)
+    while (ld > 0)
     {
-        lbin += (l%2)*i;
-        l = l/2;
+        lbin += (ld%2)*i;
+        ld /= 2;
         i *= 10;
     }
     return lbin;
 }
 
-int dectobin(int l)
+void dectobin(int ld)
 {
     int i = 0;
     int tab[8];
-    
-    while (l
+
+	while(ld > 0)
+	{
+		tab[i++]=ld%2;
+		ld/=2;
+	}
+
+	for(int j=i-1;j>=0;j--)
+		cout<<tab[j];
 }
+
+int bin2dec(int lb)
+{ 
+    int ldec = 0;
+    int i = 1; 
+    
+    while (lb > 0) 
+    {
+        ldec += (lb%10)*i; 
+        i = i*2; 
+        lb /= 10;
+    } 
+    return ldec; 
+} 
 
 int main(int argc, char **argv)
 {
-    int l = 0;
+    int ld = 0;
 	cout << "Podaj dowolną liczbę dziesiętną: ";
-    cin >> l;
+    cin >> ld;
     
-    cout << "liczba binarna: " << dec2bin(l) << endl;
+    int lb = 0;
+	cout << "Podaj dowolną liczbę binarną: ";
+    cin >> lb;
+        
+    cout << ld << " w systemie binarnym (dec2bin): " << dec2bin(ld) << endl;
+    cout << ld << " w systemie binarnym (dectobin): ";
+	dectobin(ld);
+	cout << endl;
+	cout << lb << " w systemie dziesiątkowym (bin2dec): " << dec2bin(lb) << endl; 
 	return 0;
 }
 
